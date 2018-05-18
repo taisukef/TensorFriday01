0ae826d12808d828e74de3d9f5e9e77770a9b625
Taisuke Fukuno <fukuno@TPC0129.local>
2018-05-19 07:39:55 +0900

    add DeepLearing

--- NNlearning.py
+++ NNlearning.py
@@ -8,31 +8,31 @@ import matplotlib.pyplot as plt
 
 # ファイルからndarrayを取得
 def open_with_numpy_loadtxt(filename):
-    data = np.loadtxt(filename, delimiter=',')
-    return data
+	data = np.loadtxt(filename, delimiter=',')
+	return data
 
 x = tf.placeholder("float",[None, 4]) # 入力層
 y_ = tf.placeholder("float",[None, 6]) # 出力層
 
+num = 20
+
 # Tensorのsize mean: 平均 stdev: 標準偏差
-w_h = tf.Variable(tf.random_normal([4, 10], mean=0.0, stddev=0.05))
-w_o = tf.Variable(tf.random_normal([10, 6], mean=0.0, stddev=0.05))
-b_h = tf.Variable(tf.zeros([10]))
+w_h = tf.Variable(tf.random_normal([4, num], mean=0.0, stddev=0.05))
+b_h = tf.Variable(tf.zeros([num]))
+w_o = tf.Variable(tf.random_normal([num, 6], mean=0.0, stddev=0.05))
 b_o = tf.Variable(tf.zeros([6]))
 
 # model
 def model(X, w_h, b_h, w_o, b_o):
-    # matmulは行列の掛け算をしてくれる
-    # sigmoidは活性化関数
-    h = tf.sigmoid(tf.matmul(X, w_h) + b_h)
-    # Reluで活性化したい場合
-    # h = tf.nn.relu(tf.matmul(x, w_h) + b_h)
-    
-    # softmax関数は与えたデータから確率っぽいものを求めてくれます
-    # softmax関数も活性化関数です
-    pyx = tf.nn.softmax(tf.matmul(h, w_o) + b_o)
+	# matmulは行列の掛け算をしてくれる
+	# sigmoidは活性化関数
+	h = tf.sigmoid(tf.matmul(X, w_h) + b_h)
+	# Reluで活性化したい場合
+	#h = tf.nn.relu(tf.matmul(x, w_h) + b_h)
 
-    return pyx
+	# softmax関数は与えたデータから確率っぽいものを求めてくれます
+	# softmax関数も活性化関数です
+	return tf.nn.softmax(tf.matmul(h, w_o) + b_o)
 
 y_hypo = model(x, w_h, b_h, w_o, b_o)
 
@@ -61,10 +61,8 @@ testData = tf.convert_to_tensor(open_with_numpy_loadtxt("testRGBYB.csv"))
 testLabel = tf.convert_to_tensor(open_with_numpy_loadtxt("testLabelsRGBYB.csv"))
 
 
-saver = tf.train.Saver()
-
 """
-ここから    dataとlabelをshuffe
+ここから	dataとlabelをshuffe
 """
 data_range = range(len(data))
 range_list = list(data_range)
@@ -74,9 +72,9 @@ r_train_list = []
 r_train_labels = []
 
 for t in data_range:
-    listNum = range_list[t]
-    r_train_list.append(list(data[listNum]))
-    r_train_labels.append(list(labels[listNum]))
+	listNum = range_list[t]
+	r_train_list.append(list(data[listNum]))
+	r_train_labels.append(list(labels[listNum]))
 
 
 data = r_train_list
@@ -88,41 +86,46 @@ labels = tf.convert_to_tensor(labels)
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
-    # 初期化
-    sess.run(tf.global_variables_initializer())
-    batch_xs = data.eval()
-    batch_ys = labels.eval()
+	# 初期化
+	sess.run(tf.global_variables_initializer())
+	batch_xs = data.eval()
+	batch_ys = labels.eval()
 
-    # 分類結果の判定
-    correct_prediction = tf.equal(tf.argmax(y_hypo,1), tf.argmax(y_,1))
-    # 精度の計算
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+	# 分類結果の判定
+	correct_prediction = tf.equal(tf.argmax(y_hypo,1), tf.argmax(y_,1))
+	# 精度の計算
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-    # セーブした変数を復元
-    # saver.restore(sess, "/tmp/model.ckpt")
-    # print("Model restored.")
+	# セーブした変数を復元
+	# saver.restore(sess, "./model.ckpt")
+	# print("Model restored.")
 
-    for i in range(400):
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	for i in range(500):
+		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-        if i % 100 == 0:
-            train_accuracy = accuracy.eval({x: batch_xs, y_: batch_ys})
-            print('  step, accuracy = %6d: %6.3f' % (i, train_accuracy))
+		if i % 100 == 0:
+			train_accuracy = accuracy.eval({x: batch_xs, y_: batch_ys})
+			print('  step, accuracy = %6d: %6.3f' % (i, train_accuracy))
 
-    print("学習結果")
-    print("test dataでの精度")
-    print(accuracy.eval({x: testData.eval(), y_: testLabel.eval()}) * 100)
+	print("学習結果")
+	print("test dataでの精度")
+	print(accuracy.eval({x: testData.eval(), y_: testLabel.eval()}) * 100)
 
-    # 変数データを保存する
-    # save_path = saver.save(sess, "./model.ckpt")
-    # print("Model saved in file: %s" % save_path)
+	# 変数データを保存する
+	save_path = saver.save(sess, "./model.ckpt")
+	print("Model saved in file: %s" % save_path)
 
-    testData = tf.cast(testData.eval(), tf.float32)
-    y_hypo = model(testData.eval(), w_h, b_h, w_o, b_o)
-    y_hypo = tf.cast(y_hypo.eval(), tf.float64)
+	testData = tf.cast(testData.eval(), tf.float32)
+	y_hypo = model(testData.eval(), w_h, b_h, w_o, b_o)
+	y_hypo = tf.cast(y_hypo.eval(), tf.float64)
 
-    result = tf.nn.softmax(y_hypo.eval())
-    print("result:")
-    # 一番大きい値のindexがその判定した色の番号
-    print(result[0:10].eval())
+	result = tf.nn.softmax(y_hypo.eval())
+	print("result:")
+	# 一番大きい値のindexがその判定した色の番号
+	print(testData[0:10].eval())
+	print(result[0:10].eval())
 
+	print("バイアスの値")
+	print(b_h.eval())
+	print("重みの値")
+	print(w_h.eval())
